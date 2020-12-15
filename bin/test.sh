b875e1e83d8b67298f3c7301b4ca07c30633892c
Daniel Crowe <crowe.daniel@gmail.com>
2020-12-15 20:05:12 +1030

    Whoops fix in macos

--- bin/test.sh
+++ bin/test.sh
@@ -5,4 +5,4 @@ dotnet test --collect:"XPlat Code Coverage"
 dotnet reportgenerator "-reports:tests/**/coverage.cobertura.xml" -targetdir:htmlcov -verbosity:Warning
 
 # codacy reporter doesn't support globs, so copy it to knwon location
-cp tests/**/coverage.cobertura.xml ./coverage.xml
+cp tests/VisualRegressionTracker.Tests/TestResults/*/coverage.cobertura.xml ./coverage.xml
